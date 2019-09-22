b80da49061fcf2106274cbae919b28bd0dba80f6
dSoultanis <soulprovider.gr@gmail.com>
2019-09-22 19:16:38 +0300

    *

--- config-salt.sh
+++ config-salt.sh
@@ -1,2 +1,5 @@
-wget -O/usr/lib/python2.7/dist-packages/salt/utils/systemd.py
-/usr/lib/python2.7/dist-packages/salt/modules/glusterfs.py
+#!/bin/sh -
+wget https://raw.githubusercontent.com/dsoultanis/salt-bootstrap/develop/files/systemd.py -O /usr/lib/python2.7/dist-packages/salt/utils/systemd.py
+wget https://raw.githubusercontent.com/dsoultanis/salt-bootstrap/develop/files/glusterfs.py -O /usr/lib/python2.7/dist-packages/salt/modules/glusterfs.py
+wget https://raw.githubusercontent.com/dsoultanis/salt-bootstrap/develop/files/minion.conf -O /etc/salt/minion
+
